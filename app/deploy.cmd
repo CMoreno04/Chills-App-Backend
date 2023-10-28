62e27f12e9360c8b4ebbbb344cc26067fc5b2f09
Cesar Moreno <cesar.jos.em26@icloud.com>
2023-10-27 21:56:50 -0400

    update deploy.cmd

--- app/deploy.cmd
+++ app/deploy.cmd
@@ -1,41 +1,44 @@
 @echo off
-setlocal
+SETLOCAL ENABLEDELAYEDEXPANSION
 
-REM Define variables
-set SQL_FILE_PATH=.\sql\chillisdb.sql
-set MYSQL_CONTAINER=app-db-mysql-chillis-1
-set MYSQL_PASSWORD=rootroot
+SET SQL_FILE_PATH=.\sql\chillisdb.sql
+SET MYSQL_CONTAINER=app-db-mysql-chillis-1
+SET MYSQL_PASSWORD=rootroot
 
-REM Step 1: Pull the latest code
+:: Step 1: Pull the latest code
 echo Pulling latest code from the repository...
 git pull origin main
 
-REM Step 2: Build the Maven project
+:: Step 2: Build the Maven project
 echo Building Maven project...
 mvn clean package -DskipTests
 
-REM Step 3: Build the Docker image
+:: Step 3: Build the Docker image
 echo Building Docker image...
 docker build -t chillisrestaurant-app:latest .
 
-REM Step 4: Deploy using Docker Compose
+:: Step 4: Deploy using Docker Compose
 echo Deploying using Docker Compose...
 docker-compose up -d
 
-REM Copy the .sql file to the MySQL container
+:: Copy the .sql file to the MySQL container
 echo Copying .sql file to the MySQL container...
-docker cp "%SQL_FILE_PATH%" "%MYSQL_CONTAINER%":schema.sql
+docker cp %SQL_FILE_PATH% %MYSQL_CONTAINER%:schema.sql
 
-REM Load the schema into MySQL inside the container
+:: Waiting for MySQL to start
+echo Waiting for MySQL to start...
+timeout /T 20
+
+:: Load the schema into MySQL inside the container
 echo Loading schema into MySQL...
-docker exec -it "%MYSQL_CONTAINER%" cmd /c mysql -h 127.0.0.1 -P 3306 -u root -p%MYSQL_PASSWORD% chillisdb< schema.sql
+docker exec -it %MYSQL_CONTAINER% bash -c "mysql -u root -p%MYSQL_PASSWORD% < schema.sql"
 
-REM Cleanup: Remove the .sql file from the container
+:: Cleanup: Remove the .sql file from the container
 echo Cleaning up: Removing .sql file from container...
-docker exec -it "%MYSQL_CONTAINER%" cmd /c del schema.sql
+docker exec -it %MYSQL_CONTAINER% rm schema.sql
 
 echo Schema loaded successfully!
 
 echo Pipeline completed successfully!
 
-endlocal
+ENDLOCAL
