816557008f184e444524c603dc62c2d474f7aac7
Cesar Moreno <cesar.jos.em26@icloud.com>
2023-10-31 15:58:29 -0400

    adds sudo to sh

--- deploy.sh
+++ deploy.sh
@@ -18,26 +18,26 @@ mvn clean package -DskipTests
 
 # Step 3: Build the Docker image
 echo "Building Docker image..."
-docker build -t chillisrestaurant-app:latest .
+sudo docker build -t chillisrestaurant-app:latest .
 
 # Step 4: Deploy using Docker Compose
 echo "Deploying using Docker Compose..."
-docker-compose up -d
+sudo docker-compose up -d
 
 # Copy the .sql file to the MySQL container
 echo "Copying .sql file to the MySQL container..."
-docker cp "$SQL_FILE_PATH" "$MYSQL_CONTAINER":schema.sql
+sudo docker cp "$SQL_FILE_PATH" "$MYSQL_CONTAINER":schema.sql
 
 echo "Waiting for MySQL to start..."
-sleep 20   # waits for 20 seconds
+sudo sleep 20   # waits for 20 seconds
 
 # Load the schema into MySQL inside the container
 echo "Loading schema into MySQL..."
-docker exec -it "$MYSQL_CONTAINER" bash -c "mysql -u root -p$MYSQL_PASSWORD < schema.sql"
+sudo docker exec -it "$MYSQL_CONTAINER" bash -c "mysql -u root -p$MYSQL_PASSWORD < schema.sql"
 
 # Cleanup: Remove the .sql file from the container
 echo "Cleaning up: Removing .sql file from container..."
-docker exec -it "$MYSQL_CONTAINER" rm schema.sql
+sudo docker exec -it "$MYSQL_CONTAINER" rm schema.sql
 
 echo "Schema loaded successfully!"
 
