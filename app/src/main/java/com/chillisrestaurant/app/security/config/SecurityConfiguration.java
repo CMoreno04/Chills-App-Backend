9acf995f6cc8bc34a177096e0d47c53ef1ee4ff2
Cesar Moreno <cesar.jos.em26@icloud.com>
2023-11-03 16:34:36 -0400

    taylor security to proper ip

--- app/src/main/java/com/chillisrestaurant/app/security/config/SecurityConfiguration.java
+++ app/src/main/java/com/chillisrestaurant/app/security/config/SecurityConfiguration.java
@@ -42,7 +42,7 @@ public class SecurityConfiguration {
                                 .csrf(csrf -> csrf
                                                 .ignoringRequestMatchers(
                                                                 new AntPathRequestMatcher("/login"))
-                                                .ignoringRequestMatchers(new AntPathRequestMatcher("/api/v1/auth/**"))
+                                                .ignoringRequestMatchers(new AntPathRequestMatcher("/auth/**"))
                                                 .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
 
                                 // CORS Configuration
@@ -51,7 +51,7 @@ public class SecurityConfiguration {
                                 // Authorization Configuration
                                 .authorizeHttpRequests(authorize -> authorize
                                                 .requestMatchers(new AntPathRequestMatcher("/**")).permitAll()
-                                                .requestMatchers(new AntPathRequestMatcher("/api/v1/auth/**"))
+                                                .requestMatchers(new AntPathRequestMatcher("/auth/**"))
                                                 .permitAll()
                                                 .requestMatchers(new AntPathRequestMatcher("/home")).permitAll()
                                                 .requestMatchers(new AntPathRequestMatcher("/login")).permitAll()
