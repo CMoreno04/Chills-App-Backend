570920243524f67a8a66cd9b49736bec3b8d88d5
Cesar Moreno <cesar.jos.em26@icloud.com>
2023-11-23 17:57:19 -0400

    Creates MenuItemService

--- app/src/main/java/com/chillsrestaurant/app/Services/MenuItemService.java
+++ app/src/main/java/com/chillsrestaurant/app/Services/MenuItemService.java
@@ -1,27 +1,10 @@
 package com.chillsrestaurant.app.services;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.stereotype.Service;
-
 import com.chillsrestaurant.app.entities.dto.MenuItemDTO;
-import com.chillsrestaurant.app.repositories.MenuItemRepository;
 
-@Service
-public class MenuItemService {
-
-    private final MenuItemRepository MenuItemRepository;
-
-    public MenuItemService(com.chillsrestaurant.app.repositories.MenuItemRepository menuItemRepository) {
-        MenuItemRepository = menuItemRepository;
-    }
-
-    public List<MenuItemDTO> getAllProducts() {
-        List<MenuItemDTO> productsDtos = new ArrayList<>();
-
-        this.MenuItemRepository.findAll().stream().forEach(product -> productsDtos.add(new MenuItemDTO(product)));
-
-        return productsDtos;
-    }
+public interface MenuItemService {
+     public List<MenuItemDTO> getAllProducts();
+     public List<MenuItemDTO> deleteMenuItem(Long id);
 }
