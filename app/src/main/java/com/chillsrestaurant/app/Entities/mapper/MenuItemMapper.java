d720b43277354075228ace1328ef944df3f32e17
Cesar Moreno <cesar.jos.em26@icloud.com>
2023-11-30 21:37:05 -0400

    split image string

--- app/src/main/java/com/chillsrestaurant/app/Entities/mapper/MenuItemMapper.java
+++ app/src/main/java/com/chillsrestaurant/app/Entities/mapper/MenuItemMapper.java
@@ -4,7 +4,6 @@ import java.util.Base64;
 
 import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 import com.chillsrestaurant.app.entities.MenuItem;
@@ -13,15 +12,17 @@ import com.chillsrestaurant.app.entities.dto.NewMenuItemDTO;
 @Mapper(componentModel = "spring")
 public interface MenuItemMapper {
 
-    @Mapping(target = "imageBlob", ignore = true) // Ignore automatic mapping
-    @Mapping(target = "id", ignore = true)
-    MenuItem newMenuItemDtoToMenuItem(NewMenuItemDTO menuItemDto);
-
     @AfterMapping
     default void handleImageMapping(NewMenuItemDTO menuItemDto, @MappingTarget MenuItem menuItem) {
         String imageBase64 = menuItemDto.getImage();
         if (imageBase64 != null && !imageBase64.isEmpty()) {
-            menuItem.setImageBlob(Base64.getDecoder().decode(imageBase64.split(",")[1]));
+            // Splitting the Base64 string to remove data URL prefix if present
+            String[] parts = imageBase64.split(",");
+            if (parts.length > 1) {
+                byte[] decodedBytes = Base64.getDecoder().decode(parts[1]);
+                menuItem.setImageBlob(decodedBytes);
+            }
         }
     }
 }
+
