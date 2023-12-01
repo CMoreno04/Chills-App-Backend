51e545a184f22d297208a9d6767f8789d6e51f9a
Cesar Moreno <cesar.jos.em26@icloud.com>
2023-11-30 21:41:23 -0400

    add mapper

--- app/src/main/java/com/chillsrestaurant/app/Entities/mapper/MenuItemMapper.java
+++ app/src/main/java/com/chillsrestaurant/app/Entities/mapper/MenuItemMapper.java
@@ -4,6 +4,7 @@ import java.util.Base64;
 
 import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 import com.chillsrestaurant.app.entities.MenuItem;
@@ -11,6 +12,10 @@ import com.chillsrestaurant.app.entities.dto.NewMenuItemDTO;
 
 @Mapper(componentModel = "spring")
 public interface MenuItemMapper {
+
+    @Mapping(target = "imageBlob", ignore = true) // Ignore automatic mapping
+    @Mapping(target = "id", ignore = true)
+    MenuItem newMenuItemDtoToMenuItem(NewMenuItemDTO menuItemDto);
 
     @AfterMapping
     default void handleImageMapping(NewMenuItemDTO menuItemDto, @MappingTarget MenuItem menuItem) {
@@ -25,4 +30,3 @@ public interface MenuItemMapper {
         }
     }
 }
-
