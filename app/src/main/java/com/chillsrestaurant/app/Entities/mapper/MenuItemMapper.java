a45c28b7bb9b37a6a3c93ed35ccd0459a620b186
Cesar Moreno <cesar.jos.em26@icloud.com>
2023-11-30 22:22:12 -0400

    convert image to blob

--- app/src/main/java/com/chillsrestaurant/app/Entities/mapper/MenuItemMapper.java
+++ app/src/main/java/com/chillsrestaurant/app/Entities/mapper/MenuItemMapper.java
@@ -1,11 +1,9 @@
 package com.chillsrestaurant.app.entities.mapper;
 
-import java.nio.charset.StandardCharsets;
-
-import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.AfterMapping;
 
 import com.chillsrestaurant.app.entities.MenuItem;
 import com.chillsrestaurant.app.entities.dto.NewMenuItemDTO;
@@ -13,17 +11,21 @@ import com.chillsrestaurant.app.entities.dto.NewMenuItemDTO;
 @Mapper(componentModel = "spring")
 public interface MenuItemMapper {
 
-    @Mapping(target = "imageBlob", ignore = true) // Ignore automatic mapping
-    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "imageBlob", ignore = true) // Ignore automatic mapping for imageBlob
+    @Mapping(target = "id", ignore = true) // Ignore automatic mapping for id
     MenuItem newMenuItemDtoToMenuItem(NewMenuItemDTO menuItemDto);
 
     @AfterMapping
     default void handleImageMapping(NewMenuItemDTO menuItemDto, @MappingTarget MenuItem menuItem) {
-        String imageBinaryString = menuItemDto.getImage();
-        if (imageBinaryString != null && !imageBinaryString.isEmpty()) {
-            byte[] decodedBytes = imageBinaryString.getBytes(StandardCharsets.ISO_8859_1);
-            menuItem.setImageBlob(decodedBytes);
+        String imageBase64 = menuItemDto.getImage();
+        if (imageBase64 != null && !imageBase64.isEmpty()) {
+            // Split the Base64 string to remove the data URL prefix, if present
+            String[] parts = imageBase64.split(",");
+            if (parts.length > 1) {
+                // Convert Base64 string to a byte array
+                byte[] decodedBytes = java.util.Base64.getDecoder().decode(parts[1]);
+                menuItem.setImageBlob(decodedBytes);
+            }
         }
     }
-    
 }
